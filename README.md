cc2f62ec1c43261cd31840c6339f6e5f32119dc5
nmknco <nmknco@gmail.com>
2014-08-11 15:29:06 -0400

    New folder added, and readme modified

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Test1
 
 Edit test
 
-Edit 2
+Edit 2-1 (this is edited in nb2)
 
 Edit 3
+
+Eidt 4 (this is added in nb2)
--- pyfile.py
+++ pyfile.py
@@ -1,3 +1,6 @@
 # This is a py test file created in the newbranch of Test1
 
-print "testtest"
+print "testtest - nb2"
+
+def function1():
+    print "functionx" 
